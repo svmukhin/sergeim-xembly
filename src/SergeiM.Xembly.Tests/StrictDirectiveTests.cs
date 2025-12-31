f5248b3edf084b59303291c1bc2c6f2627608646
Sergei Mukhin <sergei_mukhin@outlook.com>
2025-12-31 17:39:07 +0300

    feat: added strict directive

---
+++ src/SergeiM.Xembly.Tests/StrictDirectiveTests.cs
@@ -0,0 +1,126 @@
+// SPDX-FileCopyrightText: Copyright (c) [2025] [Sergei Mukhin]
+// SPDX-License-Identifier: MIT
+
+using System.Xml;
+
+namespace SergeiM.Xembly.Tests;
+
+[TestClass]
+public class StrictDirectiveTests
+{
+    [TestMethod]
+    public void TestStrictPassesWithNodes()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        document.AppendChild(root);
+        new StrictDirective().Execute(new Cursor(document, [root]));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(StrictException))]
+    public void TestStrictThrowsWithEmptyCursor()
+    {
+        new StrictDirective().Execute(new Cursor(new XmlDocument(), []));
+    }
+
+    [TestMethod]
+    public void TestStrictWithExactCount()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        var child1 = document.CreateElement("child1");
+        var child2 = document.CreateElement("child2");
+        document.AppendChild(root);
+        root.AppendChild(child1);
+        root.AppendChild(child2);
+        new StrictDirective(2).Execute(new Cursor(document, [child1, child2]));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(StrictException))]
+    public void TestStrictWithWrongCountThrows()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        document.AppendChild(root);
+        new StrictDirective(2).Execute(new Cursor(document, [root]));
+    }
+
+    [TestMethod]
+    public void TestStrictWithZeroCountExpected()
+    {
+        new StrictDirective(0).Execute(new Cursor(new XmlDocument(), []));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(StrictException))]
+    public void TestStrictExpectingZeroButHasNodes()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        document.AppendChild(root);
+        new StrictDirective(0).Execute(new Cursor(document, [root]));
+    }
+
+    [TestMethod]
+    public void TestStrictInWorkflow()
+    {
+        var xml = new Xembler(new Directives()
+            .Add("root")
+            .Strict(1)
+            .Add("child")
+            .Strict()
+            .Set("value")).Xml();
+        Assert.IsTrue(xml.Contains("<child>value</child>"));
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(StrictException))]
+    public void TestStrictFailsInWorkflow()
+    {
+        new Xembler(new Directives()
+            .Add("root")
+            .XPath("nonexistent")
+            .Strict()).Xml();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void TestExecuteWithNullCursorThrows()
+    {
+        new StrictDirective().Execute(null!);
+    }
+
+    [TestMethod]
+    public void TestToStringNoCount()
+    {
+        var str = new StrictDirective().ToString();
+        Assert.AreEqual("STRICT", str);
+    }
+
+    [TestMethod]
+    public void TestToStringWithCount()
+    {
+        var str = new StrictDirective(3).ToString();
+        Assert.AreEqual("STRICT 3", str);
+    }
+
+    [TestMethod]
+    public void TestStrictExceptionMessage()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        document.AppendChild(root);
+        try
+        {
+            new StrictDirective(5).Execute(new Cursor(document, [root]));
+            Assert.Fail("Should have thrown StrictException");
+        }
+        catch (StrictException ex)
+        {
+            Assert.IsTrue(ex.Message.Contains("Expected 5"));
+            Assert.IsTrue(ex.Message.Contains("has 1"));
+        }
+    }
+}
---
+++ src/SergeiM.Xembly/StrictDirective.cs
@@ -0,0 +1,51 @@
+// SPDX-FileCopyrightText: Copyright (c) [2025] [Sergei Mukhin]
+// SPDX-License-Identifier: MIT
+
+namespace SergeiM.Xembly;
+
+/// <summary>
+/// STRICT directive - validates that cursor has the expected number of nodes.
+/// </summary>
+/// <remarks>
+/// Syntax: STRICT 'count' or STRICT
+/// Throws an exception if the cursor doesn't have exactly the specified number of nodes.
+/// If no count is specified, throws if cursor has zero nodes.
+/// </remarks>
+public sealed class StrictDirective : IDirective
+{
+    private readonly int _expectedCount;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StrictDirective"/> class.
+    /// </summary>
+    /// <param name="expectedCount">The expected number of nodes. Use -1 to only check for non-empty cursor.</param>
+    public StrictDirective(int expectedCount = -1)
+    {
+        _expectedCount = expectedCount;
+    }
+
+    /// <inheritdoc/>
+    public void Execute(ICursor cursor)
+    {
+        ArgumentNullException.ThrowIfNull(cursor);
+        if (_expectedCount == -1)
+        {
+            if (!cursor.HasNodes)
+            {
+                throw new StrictException("Cursor has no nodes");
+            }
+        }
+        else
+        {
+            if (cursor.Count != _expectedCount)
+            {
+                throw new StrictException(
+                    $"Expected {_expectedCount} node(s), but cursor has {cursor.Count}");
+            }
+        }
+    }
+
+    /// <inheritdoc/>
+    public override string ToString() => 
+        _expectedCount == -1 ? "STRICT" : $"STRICT {_expectedCount}";
+}
