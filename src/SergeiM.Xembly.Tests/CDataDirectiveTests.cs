25eb806dfde24114ac982e341d810323ea214e46
Sergei Mukhin <sergei_mukhin@outlook.com>
2025-12-31 17:38:45 +0300

    feat: added cdata directive

---
+++ src/SergeiM.Xembly.Tests/CDataDirectiveTests.cs
@@ -0,0 +1,120 @@
+// SPDX-FileCopyrightText: Copyright (c) [2025] [Sergei Mukhin]
+// SPDX-License-Identifier: MIT
+
+using System.Xml;
+
+namespace SergeiM.Xembly.Tests;
+
+[TestClass]
+public class CDataDirectiveTests
+{
+    [TestMethod]
+    public void TestSetCDataOnSingleNode()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        document.AppendChild(root);
+        new CDataDirective("test data").Execute(new Cursor(document, [root]));
+        Assert.AreEqual(1, root.ChildNodes.Count);
+        Assert.AreEqual(XmlNodeType.CDATA, root.ChildNodes[0]!.NodeType);
+        Assert.AreEqual("test data", root.ChildNodes[0]!.Value);
+    }
+
+    [TestMethod]
+    public void TestCDataReplacesExistingContent()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        root.InnerText = "old text";
+        document.AppendChild(root);
+        new CDataDirective("new data").Execute(new Cursor(document, [root]));
+        Assert.AreEqual(1, root.ChildNodes.Count);
+        Assert.AreEqual(XmlNodeType.CDATA, root.ChildNodes[0]!.NodeType);
+        Assert.AreEqual("new data", root.ChildNodes[0]!.Value);
+    }
+
+    [TestMethod]
+    public void TestCDataWithSpecialCharacters()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        document.AppendChild(root);
+        new CDataDirective("<>&\"'").Execute(new Cursor(document, [root]));
+        var cdata = root.ChildNodes[0] as XmlCDataSection;
+        Assert.IsNotNull(cdata);
+        Assert.AreEqual("<>&\"'", cdata.Value);
+        Assert.IsTrue(document.OuterXml.Contains("<![CDATA[<>&\"']]>"));
+    }
+
+    [TestMethod]
+    public void TestCDataOnMultipleNodes()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        var child1 = document.CreateElement("child1");
+        var child2 = document.CreateElement("child2");
+        document.AppendChild(root);
+        root.AppendChild(child1);
+        root.AppendChild(child2);
+        new CDataDirective("same data").Execute(new Cursor(document, [child1, child2]));
+        Assert.AreEqual(XmlNodeType.CDATA, child1.ChildNodes[0]!.NodeType);
+        Assert.AreEqual(XmlNodeType.CDATA, child2.ChildNodes[0]!.NodeType);
+        Assert.AreEqual("same data", child1.ChildNodes[0]!.Value);
+        Assert.AreEqual("same data", child2.ChildNodes[0]!.Value);
+    }
+
+    [TestMethod]
+    public void TestCDataWithEmptyString()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        document.AppendChild(root);
+        new CDataDirective("").Execute(new Cursor(document, [root]));
+        Assert.AreEqual(1, root.ChildNodes.Count);
+        Assert.AreEqual(XmlNodeType.CDATA, root.ChildNodes[0]!.NodeType);
+        Assert.AreEqual("", root.ChildNodes[0]!.Value);
+    }
+
+    [TestMethod]
+    public void TestCDataClearsChildren()
+    {
+        var document = new XmlDocument();
+        var root = document.CreateElement("root");
+        var child1 = document.CreateElement("child1");
+        var child2 = document.CreateElement("child2");
+        document.AppendChild(root);
+        root.AppendChild(child1);
+        root.AppendChild(child2);
+        new CDataDirective("data").Execute(new Cursor(document, [root]));
+        Assert.AreEqual(1, root.ChildNodes.Count);
+        Assert.AreEqual(XmlNodeType.CDATA, root.ChildNodes[0]!.NodeType);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void TestConstructorWithNullValueThrows()
+    {
+        _ = new CDataDirective(null!);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void TestExecuteWithNullCursorThrows()
+    {
+        new CDataDirective("data").Execute(null!);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(CursorException))]
+    public void TestExecuteWithEmptyCursorThrows()
+    {
+        new CDataDirective("data").Execute(new Cursor(new XmlDocument(), []));
+    }
+
+    [TestMethod]
+    public void TestToString()
+    {
+        var str = new CDataDirective("test").ToString();
+        Assert.AreEqual("CDATA 'test'", str);
+    }
+}
---
+++ src/SergeiM.Xembly/CDataDirective.cs
@@ -0,0 +1,47 @@
+// SPDX-FileCopyrightText: Copyright (c) [2025] [Sergei Mukhin]
+// SPDX-License-Identifier: MIT
+
+using System.Xml;
+
+namespace SergeiM.Xembly;
+
+/// <summary>
+/// CDATA directive - sets CDATA section as node content.
+/// </summary>
+/// <remarks>
+/// Syntax: CDATA 'text'
+/// Creates a CDATA section with the specified text and sets it as the content of current nodes.
+/// </remarks>
+public sealed class CDataDirective : IDirective
+{
+    private readonly string _value;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="CDataDirective"/> class.
+    /// </summary>
+    /// <param name="value">The CDATA text value.</param>
+    /// <exception cref="ArgumentNullException">Thrown when value is null.</exception>
+    public CDataDirective(string value)
+    {
+        _value = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    /// <inheritdoc/>
+    public void Execute(ICursor cursor)
+    {
+        ArgumentNullException.ThrowIfNull(cursor);
+        if (!cursor.HasNodes)
+        {
+            throw new CursorException("Cannot set CDATA: cursor has no current nodes");
+        }
+        foreach (var node in cursor.Nodes)
+        {
+            node.RemoveAll();
+            var cdata = cursor.Document.CreateCDataSection(_value);
+            node.AppendChild(cdata);
+        }
+    }
+
+    /// <inheritdoc/>
+    public override string ToString() => $"CDATA '{_value}'";
+}
